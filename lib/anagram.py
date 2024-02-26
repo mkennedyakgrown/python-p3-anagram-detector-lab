7c444b72ee949ac919da03fd38d3c990f921bcf2
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2024-02-26 10:50:55 -0600

    Completed Lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    '''Class Anagram in anagram.py'''
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, candidates):
+        return [candidate
+                for candidate in candidates
+                if self.word.lower() != candidate.lower()
+                and sorted(self.word.lower()) == sorted(candidate.lower())]
